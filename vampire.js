3b8d20184ac061f996af1fd64d9cc2f95cc447b3
Justin Lum <j.lum07@gmail.com>
2018-05-10 20:09:13 +0000

    ex1

--- vampire.js
+++ vampire.js
@@ -10,22 +10,29 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal ? true : false);
   }
 
   /** Stretch **/
@@ -37,6 +44,48 @@ class Vampire {
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
 
+    function pathToRoot(v) {
+      let path = [v];
+      let currentVamp = v;
+      while (currentVamp.creator != null) {
+        path.unshift(currentVamp.creator);
+        currentVamp = currentVamp.creator;
+      }
+      return path;
+    }
+
+    let thisPath = pathToRoot(this);
+    let vampPath = pathToRoot(vampire);
+
+    // console.log("this", thisPath);
+    // console.log("vamp", vampPath);
+
+
+    let prevVamp = thisPath[0];
+
+    let shortPath = (thisPath.length <= vampPath.length ? thisPath.length : vampPath.length)
+
+    for (let i = 0; i < shortPath; i++) {
+      // console.log("this", thisPath[i].name, "vamp", vampPath[i].name);
+      // console.log(i);
+      // console.log("vamp", vampPath[i].name);
+      if (thisPath[i].name != vampPath[i].name) {
+        return prevVamp;
+      } else if (((i + 1 == thisPath.length) && thisPath[i].name == vampPath[i].name) || ((i + 1 == vampPath.length) && thisPath[i].name == vampPath[i].name)) {
+        return thisPath[i];
+      }
+
+      prevVamp = thisPath[i];
+
+    }
+
+    return thisPath ? this : vampire;
+
+    // if (this.creator === null) {
+    //   return this;
+    // } else if (vampire.creator === null) {
+    //   return vampire;
+    // }
   }
 }
 
